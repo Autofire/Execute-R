112ff0b1b15fe6658aa9517ecc0235740bd78b61
Code_Cube <therealcodecube@gmail.com>
2019-10-04 15:47:49 -0700

    Added helper method for animating grid dwellers.

--- Assets/Scripts/GridDweller.cs
+++ Assets/Scripts/GridDweller.cs
@@ -6,6 +6,9 @@ public class GridDweller : MonoBehaviour {
     public DwellerType type = DwellerType.Player;
     GridWorld dwellsOn;
     private CellPosition position = null;
+    private float animTimer = 0.0f, animDuration = 0.0f, animDelay = 0.0f;
+    private bool animating = false;
+    Vector3 animStart, animEnd;
 
     // Start is called before the first frame update
     void Start() {
@@ -14,7 +17,30 @@ public class GridDweller : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        animTimer += Time.deltaTime;
+        if (animating && animTimer > animDelay) {
+            // We only need to animate the X and Z axes, so make the code agnostic to any changes
+            // in the Y value.
+            if (animTimer < (animDelay + animDuration)) {
+                float progress = (animTimer - animDelay) / animDuration;
+                float oldY = gameObject.transform.position.y;
+                gameObject.transform.position = Vector3.Lerp(animStart, animEnd, progress);
+                SyncCellPositionToRealPosition();
+            } else {
+                animEnd.y = gameObject.transform.position.y;
+                gameObject.transform.position = animEnd;
+                animating = false;
+            }
+        }
+    }
+
+    public void AnimateToCell(CellPosition position, float delay, float duration) {
+        animTimer = 0.0f;
+        animDelay = delay;
+        animDuration = duration;
+        animStart = gameObject.transform.position;
+        animEnd = dwellsOn.GetRealPosition(position);
+        animating = true;
     }
 
     /// Moves the real (screen) position of this object to whatever its position in the grid world
