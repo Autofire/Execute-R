8165f5297f5dfd10a282a18a85db8d8faa2e63db
Anthony Nguyen <anthonynguyen981@gmail.com>
2019-11-04 13:01:25 -0800

    Gun Fires Bullet

--- Assets/Scripts/VR/DefaultGun.cs
+++ Assets/Scripts/VR/DefaultGun.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class DefaultGun : AbstractGun
 {
+    public Transform barrelPoint;
+    public GameObject bulletPrefab;
+
     public override void Fire()
     {
         Debug.Log("Gun Fired");
+        Instantiate(bulletPrefab, barrelPoint.position, barrelPoint.rotation);
         
     }
 }
