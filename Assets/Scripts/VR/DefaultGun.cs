3959918c12a51053067ff5a9649797427de6a9f5
Anthony Nguyen <anthonynguyen981@gmail.com>
2019-11-04 12:39:16 -0800

    SteamVR Gun Inputs Added

---
+++ Assets/Scripts/VR/DefaultGun.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultGun : AbstractGun
+{
+    public override void Fire()
+    {
+        Debug.Log("Gun Fired");
+        
+    }
+}
---
+++ Assets/Scripts/VR/GunInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GunInput : MonoBehaviour
+{
+    public SteamVR_Action_Boolean FireAction;
+    public SteamVR_Input_Sources handType;
+    public AbstractGun gun;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (FireAction.GetStateDown(handType))
+        {
+            Debug.Log("Fire Button Pressed");
+            gun.Fire();
+
+        }
+    }
+}
