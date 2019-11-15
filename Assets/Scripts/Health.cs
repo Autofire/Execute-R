7267793822810e6beb4b40743344e5c7711c8910
Kyle Townsend <townsend.kyle.kt@gmail.com>
2019-11-15 00:18:08 -0800

    Basic health and feedback system

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum DamageType
+{
+    Damage,
+    Heal
+}
+
+public class Health : MonoBehaviour
+{
+    private int currentHealth;
+    private int maxHealth;
+
+    public UnityEvent feedbackToTrigger;
+
+    private void Start()
+    {
+        maxHealth = 10; //Temporary; added to demonstrate health example.
+        //Currently no other way of specifying health amount through code or inspector.
+
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(DamageType affect, int amount)
+    {
+        if (affect == DamageType.Damage)
+        {
+            HitFeedback();
+
+            if (currentHealth <= amount)
+            {
+                Die();
+            }
+            currentHealth -= amount;
+
+            //Temporary; used to show current health of object.
+            Debug.Log(gameObject.name + " health: " + currentHealth);
+        }
+        else
+        {
+            if (currentHealth + amount > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+            else
+            {
+                currentHealth += amount;
+            }
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    //Apply some feedback showing the player they got hurt/ enemy got hurt
+    public void HitFeedback()
+    {
+        feedbackToTrigger.Invoke();
+    }
+}
---
+++ Assets/Scripts/HealthExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthExample : MonoBehaviour
+{
+    private Health health;
+
+    //Initialized with default values. Change in inspector
+    public KeyCode damageKey = KeyCode.Space;
+    public int damageAmount = 5;
+
+    private void Start()
+    {
+        health = gameObject.GetComponent<Health>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(damageKey))
+        {
+            health.TakeDamage(DamageType.Damage, damageAmount);
+        }
+    }
+
+    public void BasicFeedback()
+    {
+        Debug.Log(gameObject.name + " has been hit!");
+    }
+}
