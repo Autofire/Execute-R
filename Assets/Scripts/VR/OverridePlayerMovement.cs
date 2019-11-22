58c7b886df9bf66a494d00c0344689d92fc08243
Code_Cube <therealcodecube@gmail.com>
2019-11-22 13:17:11 -0800

    Merge master into joshm-enemies

---
+++ Assets/Scripts/VR/OverridePlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OverridePlayerMovement : MonoBehaviour
+{
+	public enum Axis : int {
+		X = 0,
+		Y = 1,
+		Z = 2
+	}
+
+	public float moveSpeed;
+	public string hAxisName = "Horizontal";
+	public string vAxisName = "Vertical";
+	public Axis hAxis = Axis.X;
+	public Axis vAxis = Axis.Z;
+
+	[Tooltip("This triggers the FIRST time a move is registered.")]
+	public UnityEvent onFirstMove;
+	private bool triggeredFirstMoveEvent = false;
+
+	public void Update() {
+		float hAxisVal = Input.GetAxis(hAxisName);
+		float vAxisVal = Input.GetAxis(vAxisName);
+
+		if(Mathf.Abs(hAxisVal) > Mathf.Epsilon || Mathf.Abs(vAxisVal) > Mathf.Epsilon) {
+
+			if(!triggeredFirstMoveEvent) {
+				triggeredFirstMoveEvent = true;
+				onFirstMove.Invoke();
+			}
+
+			Vector3 desiredMove = Vector3.zero;
+			desiredMove[(int) hAxis] = hAxisVal * moveSpeed * Time.deltaTime;
+			desiredMove[(int) vAxis] = vAxisVal * moveSpeed * Time.deltaTime;
+
+			transform.position += desiredMove;
+
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/VR/VRGridPosition.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ReachBeyond.VariableObjects;
+
+public class VRGridPosition : MonoBehaviour
+{
+	public TransformConstReference vrPosition;
+	public GridDweller dweller;
+
+	public float leashDistance = 1f;
+
+	public void Update() {
+		Vector3 targetPos = vrPosition.ConstValue.position;
+		targetPos.y = 0;
+
+//		Debug.Log(Vector3.Distance(targetPos, transform.position));
+
+		Vector3 distance = targetPos - transform.position;
+
+		// Ok, circular distance isn't going to work. We'll use a
+		// distance across each axis instead.
+		if(Mathf.Abs(distance.x) > leashDistance || Mathf.Abs(distance.z) > leashDistance) {
+
+			Vector3 oldPosition = transform.position;
+
+			if(dweller.GridizeRealPostion(targetPos).HasValue) {
+				SetPosition(targetPos);
+
+				Debug.Log(dweller.GetSpacePosition());
+			}
+
+		}
+
+	}
+
+	private void SetPosition(Vector3 newPos) {
+		transform.position = newPos;
+		dweller.SyncCellPositionToRealPosition();
+		dweller.SyncRealPositionToCellPosition();
+	}
+
+}
