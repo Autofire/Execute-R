9f595afbd30242887e2c99276766c39a568b19d3
Autofire <Autofire@users.noreply.github.com>
2019-11-26 10:48:44 -0800

    Imported explosion from past project.

--- Assets/Scripts/DestroySelfOnHit.cs
+++ Assets/Scripts/DestroySelfOnHit.cs
@@ -6,10 +6,15 @@ using System.Linq;
 public class DestroySelfOnHit : MonoBehaviour
 {
 	public string[] tagsToIgnore;
+	public GameObject spawnOnDestruction;
 
 	private void OnTriggerEnter(Collider other) {
 		if(!tagsToIgnore.Any(other.CompareTag)) {
 			Destroy(gameObject);
+
+			if(spawnOnDestruction != null) {
+				Instantiate(spawnOnDestruction, transform.position, spawnOnDestruction.transform.rotation);
+			}
 		}
 	}
 }
