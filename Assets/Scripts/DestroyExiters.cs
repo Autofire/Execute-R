b8d175d43165c2de55b4f29a61e1fc16be7c2534
Autofire <Autofire@users.noreply.github.com>
2019-11-26 10:17:22 -0800

    Deleted a whole BUNCH of trash. Also bullets delete themselves.

---
+++ Assets/Scripts/DestroyExiters.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyExiters : MonoBehaviour
+{
+
+	private void OnTriggerExit(Collider other) {
+		Destroy(other.gameObject);
+	}
+}
---
+++ Assets/Scripts/DestroySelfOnHit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DestroySelfOnHit : MonoBehaviour
+{
+	public string[] tagsToIgnore;
+
+	private void OnTriggerEnter(Collider other) {
+		if(!tagsToIgnore.Any(other.CompareTag)) {
+			Destroy(gameObject);
+		}
+	}
+}
