a73b0f9d1c5a8ada0554fafce7712e66402eb6d1
Autofire <Autofire@users.noreply.github.com>
2019-11-27 13:06:22 -0800

    Did the warning cell.

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour {
-    Vector3 velocity = Vector3.up;
+	public Vector3 velocity = new Vector3(0, 0, -4);
     GridDweller dweller;
 
+	/*
     public void SetVelocity(Vector3 newVelocity) {
         velocity = newVelocity;
     }
+	*/
 
     void Start() {
         dweller = GetComponent<GridDweller>();
-        SetVelocity(Vector3.back * 4.0f);
+		//SetVelocity(Vector3.back * 4.0f);
+		//velocity = Vector3.back * 4.0f;
     }
 
     void Update() {
-        gameObject.transform.Translate(velocity * Time.deltaTime);
-        if (dweller.GetGridWorld().IsTypeInCell(dweller.GetCurrentCell(), DwellerType.Player)) {
-            // TODO: Deduct health.
-            Debug.Log("HIT THE PLAYER! (TODO: DEDUCT HEALTH)");
-            Destroy(gameObject);
-        }
+		//Vector3 curPos = transform.position;
+		//dweller.SyncCellPositionToRealPosition();
+		//transform.Translate(velocity * Time.deltaTime);
+		//curPos += velocity * Time.deltaTime;
+		transform.position += velocity * Time.deltaTime;
+		if(dweller.GetGridWorld().IsTypeInCell(dweller.GetCurrentCell(), DwellerType.Player)) {
+			// TODO: Deduct health.
+			Debug.Log("HIT THE PLAYER! (TODO: DEDUCT HEALTH)");
+			Destroy(gameObject);
+		}
     }
 }
