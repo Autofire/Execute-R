1a36225c68c94f774ec436d105c60e8aee982177
Autofire <Autofire@users.noreply.github.com>
2019-11-26 16:26:35 -0800

    Playergun (#21)
    * Created AbstractGun
    
    * SteamVR Gun Inputs Added
    
    * Gun Fires Bullet
    
    * Made bullet move and non-vr fire control
    
    * Bullets
    
    Made it stop shooting hands
    
    * Tore up the VR player prefabs into more workable pieces.
    
    * Un-did silly change which caused guns to rapidfire.
    
    * Tuned the bullets and their visuals.
    
    * Bullets move themselves instead of being launched.
    
    * Added haptic feedback.
    
    * Further broke things up. Now the gun itself is its own prefab.
    
    * Deleted a whole BUNCH of trash. Also bullets delete themselves.
    
    * Imported explosion from past project.
    
    * Messed around with explosion thing.

---
+++ Assets/Scripts/Transformer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using ReachBeyond.VariableObjects;
+
+public class Transformer : MonoBehaviour {
+
+	[SerializeField] private Vector3ConstReference moveSpeed;
+	[SerializeField] private Vector3ConstReference rotationSpeed;
+	[SerializeField] private Vector3ConstReference scaleSpeed;
+
+	void Update () {
+		transform.position += moveSpeed.ConstValue * Time.deltaTime;
+		//transform.Rotate(rotationSpeed.constValue * Time.deltaTime);
+		transform.localRotation = transform.localRotation * Quaternion.Euler(rotationSpeed.ConstValue * Time.deltaTime);
+		transform.localScale += scaleSpeed.ConstValue * Time.deltaTime;
+	}
+}
